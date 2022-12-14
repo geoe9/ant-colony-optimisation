e5fa7ec6285a7fa2682e078164a009f3f028e119
George <48719453+geoe9@users.noreply.github.com>
2022-12-14 17:28:57 +0000

    Update AntColony

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class AntColony:
-    def __init__(self, number_of_ants, evaporation_rate):
+    def __init__(self, number_of_ants: int, evaporation_rate: float, data: list):
         self.number_of_ants = number_of_ants
         self.evaporation_rate = evaporation_rate
+        self.data = data
