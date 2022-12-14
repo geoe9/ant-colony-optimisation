a985cf8fc1bc109dbedf8d28634364f61f511186
George <48719453+geoe9@users.noreply.github.com>
2022-12-14 22:37:29 +0000

    Update AntColony to take data from FileReader

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ class AntColony:
     def __init__(self, number_of_ants: int, evaporation_rate: float, data: list) -> None:
         self.number_of_ants = number_of_ants
         self.evaporation_rate = evaporation_rate
-        self.data = data
+        self.number_of_nodes, self.distance_matrix, self.flow_matrix = data
+        self.pherimone_matrix = [[1 for j in range(self.number_of_nodes)] for i in range(self.number_of_nodes)]
 
 class FileReader:
+    '''
+    Class to read input file.
+    '''
     def __init__(self, file_path: str) -> None:
         with open(file_path) as f:
             file_contents = f.read().strip()
