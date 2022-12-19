2480897b0c2e8fb02826cf1c98a7de44dff13885
George <48719453+geoe9@users.noreply.github.com>
2022-12-19 02:03:26 +0000

    Basic ACO algorithm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+import numpy as np
+from tqdm import tqdm
 
 class AntColony:
     def __init__(self, number_of_ants: int, evaporation_rate: float, data: tuple[int, list[list[int], list[list[int]]]]) -> None:
@@ -6,26 +8,72 @@ class AntColony:
         self.evaporation_rate = evaporation_rate
         self.number_of_nodes, self.distance_matrix, self.flow_matrix = data
 
-        # compute initial heuristic matrix
-        self.heuristic_matrix = [[round(1 / (self.distance_matrix[i][j] * self.flow_matrix[i][j]), 4) if i != j else 0 for j in range(self.number_of_nodes)] for i in range(self.number_of_nodes)]
         # initalise pheromone matrix
-        self.pheromone_matrix = [[1 for j in range(self.number_of_nodes)] for i in range(self.number_of_nodes)]
+        self.pheromone_matrix = [[np.random.random() if i != j else 0 for j in range(self.number_of_nodes + 1)] for i in range(self.number_of_nodes + 1)]
     
-    def run(self):
-        self.number_of_fitness_evaluations = 0
-        if self.number_of_fitness_evaluations <= 10_000:
-            # loop
-            pass
+    def run(self, iterations = 10_000):
+        ants = [Ant(self) for _ in range(self.number_of_ants)]
+        best_fitness = None
+        best_path = None
+        progress_bar = tqdm(range(iterations))
+        for i in progress_bar:
+            for ant in ants:
+                path = ant.calculatePath()
+                fitness = ant.calculatePathFitness()
+                if best_fitness is None or fitness < best_fitness:
+                    best_fitness = fitness
+                    best_path = path
+            for ant in ants:
+                ant.updatePheromones()
+            self.evaporatePheromones()
+            progress_bar.set_description(f'\rBest fitness:\t{best_fitness:,d}')
+        print(f'\nSimulation complete.\nBest fitness: {best_fitness:,d}\nPath: {best_path}')
+            
+    def evaporatePheromones(self):
+        self.pheromone_matrix = [np.multiply(i, self.evaporation_rate) for i in self.pheromone_matrix]
 
 class Ant:
     def __init__(self, colony: AntColony):
         self.colony = colony
 
+    def calculatePath(self) -> list:
+        self.current_node = 0
+        self.allowed_nodes = [1 if i != 0 else 0 for i in range(self.colony.number_of_nodes + 1)]
+        self.path = []
+        for i in range(self.colony.number_of_nodes):
+            self.chooseNextNode()
+        return self.path
+
+    def chooseNextNode(self) -> None:
+        next_node_weightings = np.multiply([i for i in self.colony.pheromone_matrix[self.current_node]], self.allowed_nodes)
+        next_node_probabilities = np.array(next_node_weightings) / np.sum(next_node_weightings)
+        next_node = np.random.choice(self.colony.number_of_nodes + 1, p = next_node_probabilities)
+        self.path.append(next_node)
+        self.allowed_nodes[next_node] = 0
+        self.current_node = next_node
+
+    def calculatePathFitness(self) -> float:
+        '''
+        Note that lower fitness is better in this case.
+        '''
+        self.fitness = 0
+        for i in range(len(self.path)):
+            for j in range(len(self.path)):
+                self.fitness += self.colony.distance_matrix[i][j] * self.colony.flow_matrix[self.path[i] - 1][self.path[j] - 1]
+        return self.fitness
+
+    def updatePheromones(self) -> None:
+        pheromone_amount = 100000 / self.fitness
+        self.colony.pheromone_matrix[0][self.path[0]] += pheromone_amount
+        for i in range(0, len(self.path) - 1):
+            self.colony.pheromone_matrix[self.path[i]][self.path[i+1]] += pheromone_amount
+            self.colony.pheromone_matrix[self.path[i+1]][self.path[i]] += pheromone_amount
+
 class FileReader:
     '''
     Class to read input file.
     '''
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: str):
         with open(file_path) as f:
             file_contents = f.read().strip()
             data = re.split('\n[ ]+\n', file_contents)
@@ -39,3 +87,6 @@ class FileReader:
         (Number of nodes, Distance matrix, Flow matrix)
         '''
         return self.number_of_nodes, self.distance_matrix, self.flow_matrix
+
+colony = AntColony(10, 0.9, FileReader("uni50a.dat").getData())
+colony.run()
