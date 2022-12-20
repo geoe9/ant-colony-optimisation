bdda6501126bfdb08c2ee18915239a752680bf59
George <48719453+geoe9@users.noreply.github.com>
2022-12-20 19:55:36 +0000

    Add documentation & commenting

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ class AntColony:
         # initalise progress bar to track simulations
         progress_bar = tqdm(range(fitness_evaluations), f'Running simulation (m={self.number_of_ants}, e={self.evaporation_rate})')
 
+        ant_number = 0
         for i in progress_bar:
-            ant_number = 0
             path = ants[ant_number].calculatePath()
             fitness = ants[ant_number].calculatePathFitness()
             if self.best_fitness is None or fitness < self.best_fitness:
@@ -44,6 +44,8 @@ class AntColony:
                     ant.updatePheromones()
                 ant_number = 0
                 self.evaporatePheromones
+            
+            ant_number += 1
 
         # output results once simulation complete
         print(f'Simulation complete.\nBest fitness: {self.best_fitness:,d}\nPath: {best_path}\n')           
@@ -63,7 +65,11 @@ class Ant:
         Generate a permutation.
         '''
         self.current_node = 0
+
+        # initialise allowed nodes matrix, start with all allowed apart from starting node
         self.allowed_nodes = [1 if i != 0 else 0 for i in range(self.colony.number_of_nodes + 1)]
+
+        # iteratively choose next node to generate permutation
         self.path = []
         for i in range(self.colony.number_of_nodes):
             self.chooseNextNode()
@@ -73,11 +79,22 @@ class Ant:
         '''
         Chooses next node randomly, with bias towards nodes with more pheromone.
         '''
+        # calculate node weightings with current pheromone matrix, remove nodes that have already been visited
         next_node_weightings = np.multiply([i for i in self.colony.pheromone_matrix[self.current_node]], self.allowed_nodes)
+
+        # convert weightings to probabilities
         next_node_probabilities = np.array(next_node_weightings) / np.sum(next_node_weightings)
+
+        # choose next node randomly with bias based on probabilites
         next_node = np.random.choice(self.colony.number_of_nodes + 1, p = next_node_probabilities)
+
+        # add chosen node to solution
         self.path.append(next_node)
+
+        # remove node from allowed nodes as to not be visited again
         self.allowed_nodes[next_node] = 0
+
+        # set current node to chosen node
         self.current_node = next_node
 
     def calculatePathFitness(self) -> float:
@@ -86,6 +103,8 @@ class Ant:
         Note that lower fitness is better in this case.
         '''
         self.fitness = 0
+        
+        # sum cost from each facility to every other facility to calculate fitness
         for i in range(len(self.path)):
             for j in range(len(self.path)):
                 self.fitness += self.colony.distance_matrix[i][j] * self.colony.flow_matrix[self.path[i] - 1][self.path[j] - 1]
@@ -95,7 +114,10 @@ class Ant:
         '''
         Updates the global pheromone matrix with values proportionate to fitness of the ant's generated permutation.
         '''
+        # calculate pheromone amount - less fitness is better
         pheromone_amount = 1 / self.fitness
+
+        # add pheromone to pheromone matrix
         self.colony.pheromone_matrix[0][self.path[0]] += pheromone_amount
         for i in range(0, len(self.path) - 1):
             self.colony.pheromone_matrix[self.path[i]][self.path[i+1]] += pheromone_amount
@@ -107,9 +129,17 @@ class FileReader:
     def __init__(self, file_path: str):
         with open(file_path) as f:
             file_contents = f.read().strip()
+
+            # split values/matricies
             data = re.split('\n[ ]+\n', file_contents)
+
+            # read number of facilities
             self.number_of_nodes = int(data[0])
+
+            # read distance matrix
             self.distance_matrix = [[int(y) for y in x.split()] for x in data[1].split("\n")]
+
+            # read flow matrix
             self.flow_matrix = [[int(y) for y in x.split()] for x in data[2].split("\n")]
     
     def getData(self) -> tuple[int, list[list[int]], list[list[int]]]:
@@ -124,7 +154,7 @@ if __name__ == "__main__":
     file_data = FileReader("uni50a.dat").getData()
 
     # list of simluations to run (m, e)
-    tests = [(100, 0.9), (100, 0.5), (10, 0.9), (10, 0.5)]
+    tests = [(100, 0.9)]
 
     for test in tests:
         # run each simulation 5 times
